f9c4dfc5a2bcdd60439e0f5eca305573956418c5
Babithkp <babithkullachetti44@gmail.com>
2024-01-16 07:06:25 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Form from "./components/form";
+
+
+
+function App() {
+
+  return (
+    <div className=" flex h-[100vh] justify-center items-center">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
